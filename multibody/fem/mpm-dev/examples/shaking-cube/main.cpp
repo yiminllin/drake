eca1a44138501d4b2ee3baf2f6f5c17d265c33ed
Yimin Lin <yimin.lin@tri.global>
2022-07-25 12:50:15 -0700

    shaking cube

---
+++ multibody/fem/mpm-dev/examples/shaking-cube/main.cpp
@@ -0,0 +1,121 @@
+#include <array>
+#include <cmath>
+#include <iostream>
+#include <memory>
+#include <random>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <Partio.h>
+
+#include "drake/common/drake_assert.h"
+#include "drake/common/eigen_types.h"
+#include "drake/common/filesystem.h"
+#include "drake/common/temp_directory.h"
+#include "drake/math/roll_pitch_yaw.h"
+#include "drake/multibody/fem/mpm-dev/CorotatedElasticModel.h"
+#include "drake/multibody/fem/mpm-dev/MPMDriver.h"
+#include "drake/multibody/fem/mpm-dev/SpatialVelocityTimeDependent.h"
+#include "drake/multibody/math/spatial_velocity.h"
+
+namespace drake {
+namespace multibody {
+namespace mpm {
+
+multibody::SpatialVelocity<double> oscillating_velocity(double t) {
+    multibody::SpatialVelocity<double> wall_velocity;
+    wall_velocity.rotational() = Vector3<double>(0.0, 0.0, 0.0);
+    double freq = 1.0;
+    if (static_cast<int>(trunc(t / freq)) % 2 == 0) {
+        wall_velocity.translational() = Vector3<double>(0.0, 0.0, 1.0);
+    } else {
+        
+    }
+    return wall_velocity;
+}
+
+int DoMain() {
+    MPMParameters::PhysicalParameters p_param {
+        {0.0, 0.0, -9.81}                      // Gravitational acceleration
+    };
+
+    MPMParameters::SolverParameters s_param {
+        2e-0,                                  // End time
+        5e-4,                                  // Time step size
+        0.1,                                   // Grid size
+    };
+
+    MPMParameters::IOParameters io_param {
+        "mpm-test",                            // case name
+        "/home/yiminlin/Desktop/output",       // output directory name
+        0.04,                                  // Interval of outputting
+    };
+
+    MPMParameters param {p_param, s_param, io_param};
+    auto driver = std::make_unique<MPMDriver>(std::move(param));
+
+    KinematicCollisionObjects objects = KinematicCollisionObjects();
+
+    // Initialize the left wall
+    std::unique_ptr<SpatialVelocityTimeDependent> left_wall_velocity_ptr =
+        std::make_unique<SpatialVelocityTimeDependent>(oscillating_velocity);
+    double left_wall_mu = 10.0;
+    Vector3<double> left_wall_normal = {1.0, 0.0, 0.0};
+    std::unique_ptr<AnalyticLevelSet> left_wall_level_set =
+                            std::make_unique<HalfSpaceLevelSet>(left_wall_normal);
+    Vector3<double> left_wall_translation = {0.8, 0.0, 0.0};
+    math::RigidTransform<double> left_wall_pose =
+                            math::RigidTransform<double>(left_wall_translation);
+    objects.AddCollisionObject(std::move(left_wall_level_set), std::move(left_wall_pose),
+                               std::move(left_wall_velocity_ptr), left_wall_mu);
+
+    // Initialize the right wall
+    std::unique_ptr<SpatialVelocityTimeDependent> right_wall_velocity_ptr =
+        std::make_unique<SpatialVelocityTimeDependent>(oscillating_velocity);
+    double right_wall_mu = 10.0;
+    Vector3<double> right_wall_normal = {-1.0, 0.0, 0.0};
+    std::unique_ptr<AnalyticLevelSet> right_wall_level_set =
+                            std::make_unique<HalfSpaceLevelSet>(right_wall_normal);
+    Vector3<double> right_wall_translation = {1.2, 0.0, 0.0};
+    math::RigidTransform<double> right_wall_pose =
+                            math::RigidTransform<double>(right_wall_translation);
+    objects.AddCollisionObject(std::move(right_wall_level_set), std::move(right_wall_pose),
+                               std::move(right_wall_velocity_ptr), right_wall_mu);
+
+    // Initialize a sphere
+    double radius = 0.2;
+    SphereLevelSet level_set_sphere = SphereLevelSet(radius);
+    Vector3<double> translation_sphere = {1.0, 1.0, 2.0};
+    math::RigidTransform<double> pose_sphere =
+                            math::RigidTransform<double>(translation_sphere);
+    multibody::SpatialVelocity<double> velocity_sphere;
+    velocity_sphere.translational() = Vector3<double>::Zero();
+    velocity_sphere.rotational() = Vector3<double>{0.0, 0.0, 0.0};
+
+    double E = 8e4;
+    double nu = 0.49;
+    std::unique_ptr<CorotatedElasticModel> elastoplastic_model
+            = std::make_unique<CorotatedElasticModel>(E, nu);
+    MPMDriver::MaterialParameters m_param_sphere{
+                                                std::move(elastoplastic_model),
+                                                1200,
+                                                velocity_sphere,
+                                                1
+                                                };
+
+    driver->InitializeKinematicCollisionObjects(std::move(objects));
+    driver->InitializeParticles(level_set_sphere, pose_sphere,
+                                std::move(m_param_sphere));
+    driver->DoTimeStepping();
+
+    return 0;
+}
+
+}  // namespace mpm
+}  // namespace multibody
+}  // namespace drake
+
+int main() {
+    return drake::multibody::mpm::DoMain();
+}
